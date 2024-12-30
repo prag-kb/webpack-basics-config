8310b7411c2a789e13fc8b641fccbf63ccefacb3
prag-kb <kbprabu6@gmail.com>
2024-12-30 14:35:39 +0530

    webpack config commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    open: true,
+    port: 9000,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: "defaults",
+                  },
+                ],
+                "@babel/preset-react",
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
